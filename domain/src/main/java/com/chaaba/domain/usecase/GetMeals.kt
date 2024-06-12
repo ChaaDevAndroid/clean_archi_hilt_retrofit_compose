1eddb356b74230d9038e94e883e7407e6288fa1c
id_chaaba <dev.chaaba@gmail.com>
2024-06-12 22:29:42 +0100

    delete GetMeals repeated

--- domain/src/main/java/com/chaaba/domain/usecase/GetMeals.kt
+++
@@ -1,7 +0,0 @@
-package com.chaaba.domain.usecase
-
-import com.chaaba.domain.repo.IMealRepo
-
-class GetMeals(private val _repo: IMealRepo) {
-    suspend operator fun invoke() = _repo.getMealsFromRemote()
-}
